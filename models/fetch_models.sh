7e901bdab4daacfdefad6bf779020a79e51aca34
tuanchien <tuan@sdk.org.nz>
2017-06-04 20:21:45 +1200

    Fix filename issue with fetch_models.sh

--- models/fetch_models.sh
+++ models/fetch_models.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-wget http://dlib.net/files/dlib_face_recognition_resnet_model_v1.dat.bz2
-wget http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
-wget http://dlib.net/files/mmod_human_face_detector.dat.bz2
+#wget http://dlib.net/files/dlib_face_recognition_resnet_model_v1.dat.bz2
+#wget http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
+#wget http://dlib.net/files/mmod_human_face_detector.dat.bz2
 
-bunzip2 http://dlib.net/files/dlib_face_recognition_resnet_model_v1.dat.bz2
-bunzip2 http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
-bunzip2 http://dlib.net/files/mmod_human_face_detector.dat.bz2
+bunzip2 dlib_face_recognition_resnet_model_v1.dat.bz2
+bunzip2 shape_predictor_68_face_landmarks.dat.bz2
+bunzip2 mmod_human_face_detector.dat.bz2
